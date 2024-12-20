a5347973e8a50a6522692a0dd9725fed18b0cbb1
Raghvendra Sharma <raghvendra1708@gmail.com>
2024-12-21 00:09:27 +0530

    Delete whatsApp-clone-2-main directory

--- whatsApp-clone-2-main/src/components/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import chatsReducer from '../features/chats/chatSlice.js';
-
-export const store = configureStore({
-    reducer: {
-        chats: chatsReducer
-    }
-});
